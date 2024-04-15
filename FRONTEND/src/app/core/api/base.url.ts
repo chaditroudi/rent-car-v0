4a775573f3d46de77ca94e14624420bd84d78087
chaditroudi <troudishedy6@gmail.com>
2024-04-15 19:15:10 +0200

    finish prod

--- FRONTEND/src/app/core/api/base.url.ts
+++ FRONTEND/src/app/core/api/base.url.ts
@@ -1,7 +1,13 @@
 import { HttpHeaders } from "@angular/common/http";
 
-export const baseUrl = 'http://localhost:3200/api';
+import { environment } from '../../../environments/environment.prod';
 
+
+//export const baseUrl = 'http://localhost:3200/api';
+
+
+
+export  const baseUrl= environment.API_URL;
 
 export   const httpOptions = {
     headers: new HttpHeaders({
@@ -9,3 +15,4 @@ export   const httpOptions = {
     })
   };
   
+	
